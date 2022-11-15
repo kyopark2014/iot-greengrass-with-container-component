df0edaef20221c4bab7c02bebf3dd079a45efaf8
ksdyb.park <ksdyb@amazon.com>
2022-11-16 02:17:20 +0900

    update

--- cdk-iot-container/lib/cdk-iot-container-stack.ts
+++ cdk-iot-container/lib/cdk-iot-container-stack.ts
@@ -42,7 +42,7 @@ export class CdkIotContainerStack extends cdk.Stack {
     });
 
     // create container component - com.example.container
-    const version_container = "1.0.3"
+    const version_container = "1.0.4"
     new containerComponent(scope, "container-component", version_container)   
 
     // create local component
